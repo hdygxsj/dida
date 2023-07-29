fccc92e882b8bc3bf95d641f7239420f7e6c2175
海大一个小神经 <1161623489>
2023-07-29 19:55:54 +0800

    后端初始化

--- dida-api/src/main/java/com/hdygxsj/dida/ApiApplicationServer.java
+++ dida-api/src/main/java/com/hdygxsj/dida/ApiApplicationServer.java
@@ -22,6 +22,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiApplicationServer {
 
+
     public static void main(String[] args) {
         SpringApplication.run(ApiApplicationServer.class);
     }
